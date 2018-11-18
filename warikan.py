d274961b7e4542f03a9462263e55c2a79f2a8174
Kanuka <kanukamisaki@icloud.com>
2018-11-18 09:42:08 +0900

    難しく考えすぎてしまった できない！！！

---
+++ warikan.py
@@ -0,0 +1,16 @@
+# Table: 1500円で3人
+# Table: 2000円で3人
+# Table: 3647円で4人
+
+amount = 1500
+number_of_people = 3
+
+input("テーブル番号を教えてください：　")
+
+print(f"1人あたり: {amount // number_of_people}円, 端数: {amount % number_of_people}円")
+
+amount = [table_1, table_2, table_3]
+
+table_1 = 1500
+table_2 = 2000
+table_3 = 3647
