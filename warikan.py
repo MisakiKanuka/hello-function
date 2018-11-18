c61e084b2ec2dae17e0a81086bb0e868c7ed9ca6
Kanuka <kanukamisaki@icloud.com>
2018-11-18 10:04:40 +0900

    warikan2 First commit

--- warikan.py
+++ warikan.py
@@ -2,18 +2,19 @@
 # Table: 2000円で3人
 # Table: 3647円で4人
 
-amount = 1500
-number_of_people = 3
+# amount = 1500
+# number_of_people = 3
 
-input("テーブル番号を教えてください：　")
+amount = int(input("合計金額を教えてください：　"))
+number_of_people = int(input("テーブルの人数を教えてください：　"))
 
 print(f"1人あたり: {amount // number_of_people}円, 端数: {amount % number_of_people}円")
 
-amount = [table_1, table_2, table_3]
+# amount = [table_1, table_2, table_3]
 
-table_1 = 1500
-table_2 = 2000
-table_3 = 3647
+# table_1 = 1500
+# table_2 = 2000
+#table_3 = 3647
 
 # このコードみてどうですか？
 
---
+++ warikan2.py
@@ -0,0 +1,3 @@
+print(warikan(amount=1500, number_of_people=3))
+print(warikan(amount=2000, number_of_people=3))
+print(warikan(amount=3000, number_of_people=4))
